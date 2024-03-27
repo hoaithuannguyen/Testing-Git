cce902c080968cfef10c0fb6f86abcf6ab8c0b11
tien duong <phamtienduong99@gmail.com>
2024-03-27 14:40:43 +0700

    ddd

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ const arr = [1,2,3,4,5]
 const mapped = arr.map((item)=>{
      return console.log(item)
 })
+const arr1 = [2,4,5,6,6]
+let sum = 0
+for (let i = 0; i < arr1.length; i++) {
+     sum += arr1[i]
+}
